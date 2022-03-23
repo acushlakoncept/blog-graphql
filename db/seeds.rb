5faa77c8474053fb9188d0efbd65958f9bd9dd43
Uduak Essien <acushla4real@gmail.com>
2022-03-23 11:29:04 +0100

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+  {
+    first_name: "John",
+    last_name: "Doe",
+    address: "123 Main Street",
+    postcode: "AB12 3CD",
+    city: "London",
+    country: "UK"
+  },
+  {
+    first_name: "Jane",
+    last_name: "Doe",
+    address: "24 High Street",
+    postcode: "AB12 45CD",
+    city: "New York",
+    country: "US"
+  }
+])
+
+posts = Post.create!([
+  {
+    body: "This is a post",
+    user: users.first
+  },
+  {
+    body: "This is another post",
+    user: users.first
+  },
+  {
+    body: "This is yet another post",
+    user: users.last
+  }
+])
+
+Comment.create!([
+  {
+    body: "This is a comment",
+    post: posts.first,
+    user: users.first
+  },
+  {
+    body: "This is another comment",
+    post: posts.first,
+    user: users.last
+  }
+])
